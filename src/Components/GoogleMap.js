aebdc0af2c2f8b145e39ede9e4d3d8c49aee4ceb
Minde339 <58472290+Minde339@users.noreply.github.com>
2021-03-24 20:14:38 +0200

    Added .env without API KEY Exposed

--- src/Components/GoogleMap.js
+++ src/Components/GoogleMap.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
-
-
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 
 const mapStyles = {
-  width: '100%',
-  height: '100%',
+  width: "100%",
+  height: "100%",
 };
 
 export class MapContainer extends Component {
@@ -13,38 +11,43 @@ export class MapContainer extends Component {
     super(props);
 
     this.state = {
-        stores: [{ lat: 55.665218, lng: 21.1761816 },
-            {latitude: 55.665218, longitude: 21.1761816},
-        ]
-    }
+      stores: [
+        { lat: 55.665218, lng: 21.1761816 },
+        { latitude: 55.665218, longitude: 21.1761816 },
+      ],
+    };
   }
 
   displayMarkers = () => {
     return this.state.stores.map((store, index) => {
-      return <Marker key={index} id={index} position={{
-       lat: store.latitude,
-       lng: store.longitude
-     }}
-     onClick={() => console.log("You clicked me!")} />
-    })
-  }
+      return (
+        <Marker
+          key={index}
+          id={index}
+          position={{
+            lat: store.latitude,
+            lng: store.longitude,
+          }}
+          onClick={() => console.log("You clicked me!")}
+        />
+      );
+    });
+  };
 
   render() {
     return (
-        <Map
-          google={this.props.google}
-          zoom={15}
-          style={mapStyles}
-          initialCenter={{ lat: 55.663, lng: 21.176}}
-        >
-          {this.displayMarkers()}
-        </Map>
+      <Map
+        google={this.props.google}
+        zoom={15}
+        style={mapStyles}
+        initialCenter={{ lat: 55.663, lng: 21.176 }}
+      >
+        {this.displayMarkers()}
+      </Map>
     );
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: "AIzaSyBGgd8tzEnnZKhscZYA0-uc5ojnKX0XYbk",
+  apiKey: process.env.GOOGLE_API_KEY,
 })(MapContainer);
-
-
